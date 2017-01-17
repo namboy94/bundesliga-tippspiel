98ae3237277683c29350d9f23e5bb384d74908b7
Hermann Krumrey <hermann@krumreyh.com>
2017-01-17 16:27:30 +0100

    Add Navbar

--- html/functions/templating.php
+++ html/functions/templating.php
@@ -34,5 +34,30 @@ function initialize_session() {
 	}
 }
 
+function load_navbar($page_name) {
+
+	$page_file = ($page_name == "home" ? "index.php" : $page_name . ".php");
+
+	$home_selected = ($page_name === "home" ? 'class="active"' : "");
+	$about_selected = ($page_name === "about" ? 'class="active"' : "");
+	$contact_selected = ($page_name === "contact" ? 'class="active"' : "");
+
+	$content = load_html("templates/navbar.html");
+	$content = str_replace("@HOME_SELECTED", $home_selected, $content);
+	$content = str_replace("@ABOUT_SELECTED", $about_selected, $content);
+	$content = str_replace("@CONTACT_SELECTED", $contact_selected, $content);
+
+	$theme = $_SESSION['theme']
+
+	$default_selected = ($theme === 'default' ? 'class="active"' : "");
+	$default_link = $page_file . "?theme=default";
+
+	$content = str_replace("@DEFAULT_T_SELECTED", $default_selected, $content);
+	$content = str_replace("@DEFAULT_T_LINK", $default_link, $content);
+
+	return $content
+
+}
+
 
 ?>
--- html/index.php
+++ html/index.php
@@ -3,3 +3,14 @@ Include "functions/templating.php";
 initialize_session();
 echo load_header("Bundesliga Tippspiel");
 ?>
+
+
+<body>
+	
+	<?php
+
+		echo load_navbar()
+
+	?>
+
+</body>
