23b44fbc7e3a4bb7bc1f578ed1f4af3da3d6615d
Hermann Krumrey <hermann@krumreyh.com>
2017-01-17 21:14:52 +0100

    Fix Replacing

--- html/functions/templating.php
+++ html/functions/templating.php
@@ -86,7 +86,6 @@ function fill_string_variables($content) {
 function fill_strings($content, $dictionary) {
 
 	foreach (array_keys($dictionary) as $key) {
-		echo $key;
 		$content = str_replace($key, $dictionary[$key], $content);
 	}
 	return $content;
