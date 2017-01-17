c5ca589c8feecd45087db6484b02548856fc4d0d
Hermann Krumrey <hermann@krumreyh.com>
2017-01-17 15:27:57 +0100

    Made Page Static for now

--- html/index.php
+++ html/index.php
@@ -1,8 +1,10 @@
-<?php
+<!--?php
 Include "functions/templating.php";
 initialize_session();
 echo load_header("Bundesliga Tippspiel");
-?>
+?-->
 
 
-Test
+This will be a fantastic Bundesliga Tippspiel Site one day.
+
+Check out the demo: <a href="https://demo.tippspiel.krumreyh.com">Tippspiel</a>
