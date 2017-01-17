8580286db0fb797a61c24a4385e8dbfe00b3993e
Hermann Krumrey <hermann@krumreyh.com>
2017-01-17 16:41:37 +0100

    Add Contact and About Page

---
+++ html/about.php
@@ -0,0 +1,16 @@
+<?php
+Include "functions/templating.php";
+initialize_session();
+echo load_header("Bundesliga Tippspiel - About");
+?>
+
+
+<body>
+	
+	<?php
+
+		echo load_navbar("about")
+
+	?>
+
+</body>
---
+++ html/contact.php
@@ -0,0 +1,16 @@
+<?php
+Include "functions/templating.php";
+initialize_session();
+echo load_header("Bundesliga Tippspiel - Contact");
+?>
+
+
+<body>
+	
+	<?php
+
+		echo load_navbar("contact")
+
+	?>
+
+</body>
