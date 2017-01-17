d0c3361a305106404cdbbb61e127722abf0fb6b6
Hermann Krumrey <hermann@krumreyh.com>
2017-01-17 15:29:48 +0100

    Add PHP code again

--- html/index.php
+++ html/index.php
@@ -1,10 +1,5 @@
-<!--?php
+<?php
 Include "functions/templating.php";
 initialize_session();
 echo load_header("Bundesliga Tippspiel");
-?-->
-
-
-This will be a fantastic Bundesliga Tippspiel Site one day.
-
-Check out the demo: <a href="https://demo.tippspiel.krumreyh.com">Tippspiel</a>
+?
