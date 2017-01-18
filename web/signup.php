b55090411d6249be04838207e4905cd33a834258
Hermann Krumrey <hermann@krumreyh.com>
2017-01-18 23:20:24 +0100

    Add Signup Page

---
+++ web/signup.php
@@ -0,0 +1,18 @@
+<?php
+Include "php_functions/templating.php";
+initialize_session();
+process_global_gets();
+echo load_header("signup");
+?>
+
+
+<body>
+
+<?php
+
+    echo load_navbar("signup");
+    echo load_html("html_content/templates/footer.html");
+
+?>
+
+</body>
