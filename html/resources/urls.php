56b64194b0d8bd7d248e8791ebf9bf5bd05d7942
Hermann Krumrey <hermann@krumreyh.com>
2017-01-18 09:42:28 +0100

    Fix semicolon

--- html/resources/urls.php
+++ html/resources/urls.php
@@ -8,7 +8,7 @@ function get_url_map() {
 		                   'bundesliga-tippspiel',
 		'@!DEV_EMAIL_ADDR' => 'hermann@krumreyh.com',
 		'@!DEFAULT_CSS_THEME' => "https://maxcdn.bootstrapcdn.com/" .
-		                         "bootstrap/3.3.7/css/bootstrap.min.css";
+		                         "bootstrap/3.3.7/css/bootstrap.min.css"
 
 	);
 
