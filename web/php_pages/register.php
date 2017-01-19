e80b203803018dec2012b5334f975772b58e6f97
Hermann Krumrey <hermann@krumreyh.com>
2017-01-19 14:37:00 +0100

    Try auth functions for first time

--- web/php_pages/register.php
+++ web/php_pages/register.php
@@ -1,20 +1,18 @@
 <?php
 
+Include "../php_functions/auth.php";
 
 $email = $_POST["register_email"];
 $username = $_POST["register_username"];
 $password = $_POST["register_password"];
 $repeat_password = $_POST["register_password_repeat"];
 
-echo $username . ": Password is same: ";
-
-if ($password === $repeat_password) {
-    echo "Yes.";
+if (username_exists($username)) {
+    echo "Username Exists";
 }
 else {
-    echo "No.";
+    echo "Username Does Not Exist";
 }
-
 
 
 
