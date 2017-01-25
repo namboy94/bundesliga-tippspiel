222ff7fbfd2828c8e6abc7d0babad46930ba684e
Hermann Krumrey <hermann@krumreyh.com>
2017-01-25 13:11:41 +0100

    Add betting section to home page

--- web/index.php
+++ web/index.php
@@ -22,6 +22,7 @@ include_once 'php/session.php';
 include_once 'templates/form.php';
 include_once 'templates/navbar.php';
 include_once 'templates/header.php';
+include_once 'templates/betform.php';
 include_once 'strings/dictionary.php';
 include_once 'templates/title_jumbotron.php';
 
@@ -41,6 +42,8 @@ $login_form = new Form('@$LOGIN_SECTION_TITLE', 'actions/login.php', array(
     new ConfirmationButton('@$LOGIN_SUBMIT_TITLE')
 ));
 
+$betform = new FullBetForm();
+
 generateDefaultHeaderNavbar('index.php')->echo();
 
 if (isLoggedIn()) {
@@ -55,22 +58,49 @@ processDismissableMessages();
 ?>
 <div class="container">
     <div class="row">
-        <div class="col-sm-8 text-center">
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+        <?php if (isLoggedIn()) {
+            echo '<div class="col-sm-6 text-center">';
+        }
+        else {
+            echo '<div class="col-sm-8 text-center">';
+        }?>
 
-            Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
 
-            Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+            Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+            amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+            aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+            gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+            voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+            takimata sanctus est Lorem ipsum dolor sit amet.
+
+            Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel
+            illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui
+            blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor
+            sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+            magna aliquam erat volutpat.
+
+            Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip
+            ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie
+            consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim
+            qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
 
             Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer
+
         </div>
-        <div class="col-sm-4">
-            <?php
-            if (!isLoggedIn()) {
-                $login_form->echo();
-                echo $dictionary->translate('<h5>@$OR_SIGN_UP_TEXT</h5>');
-            }
-            ?>
+        <?php
+        if (isLoggedIn()) {
+            echo '<div class="col-sm-6">';
+            $betform->echo();
+        }
+        else {
+            echo '<div class="col-sm-4">';
+            $login_form->echo();
+            echo $dictionary->translate('<h5>@$OR_SIGN_UP_TEXT</h5>');
+        }
+        ?>
         </div>
     </div>
 </div>
