9ceef01a2510af595f81c5740d6b275ac4bbf266
Hermann Krumrey <hermann@krumreyh.com>
2017-02-03 14:56:52 +0100

    Refactor match page

--- web/match.php
+++ web/match.php
@@ -17,20 +17,8 @@
     along with bundesliga-tippspiel.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-include_once 'php/gets.php';
-include_once 'php/session.php';
-include_once 'php/matchdb.php';
-include_once 'templates/navbar.php';
-include_once 'templates/header.php';
-include_once 'templates/betform.php';
-include_once 'strings/dictionary.php';
-include_once 'templates/match_jumbo.php';
-include_once 'templates/title_jumbotron.php';
+include_once 'php/page.php';
 include_once 'templates/dismissable_message.php';
-
-initializeSession();
-processGlobalGets();
-$dictionary = new Dictionary($_SESSION['language']);
 
 $match = null;
 if (isset($_GET['match_id'])) {
@@ -40,36 +28,23 @@ if ($match === null) {
     (new DismissableMessage('error', '@$MATCH_NOT_FOUND_ERROR_TITLE', '@$MATCH_NOT_FOUND_ERROR_BODY'))
         ->show('index.php');
 }
+
 $teams = getTeamsForMatch($match);
-
-(new Header('@$MATCH_TITLE'))->echo();
-
-echo '<body>';
-generateDefaultHeaderNavbar('match.php')->echo();
-
-(new MatchJumbo($match, $teams['team_one'], $teams['team_two']))->echo();
-processDismissableMessages();
-
+$title = $teams['team_one']['name'] . ' vs. ' . $teams['team_two']['name'];
 $team_one_logo = 'resources/images/logos/' . $teams['team_one']['id'] . '.gif';
 $team_two_logo = 'resources/images/logos/' . $teams['team_two']['id'] . '.gif';
 
-?>
-<div class="row">
-    <div class="col-sm-3"><img class="center-block" src="<?php echo $team_one_logo?>"></div>
-    <div class="col-sm-6">
-        <div class="jumbotron">
-            <div class="row">
-                <div class="col-sm-2"></div>
-                <div class="col-sm-2"><h1><?php echo $match['team_one_ft']?></h1></div>
-                <div class="col-sm-4"></div>
-                <div class="col-sm-2"><h1><?php echo $match['team_two_ft']?></h1></div>
-                <div class="col-sm-2"></div>
-            </div>
-        </div>
-    </div>
-    <div class="col-sm-3"><img class="center-block" src="<?php echo $team_two_logo?>"></div>
-</div>
-<?php
+$page = new Page($title, 'match.php', $title, array(), false);
 
-generateFooter('password_reset.php')->echoWithContainer();
-echo '</body>';
+$page->addStringBodyElement('<div class="row">');
+$page->addStringBodyElement('<div class="col-sm-3"><img class="center-block" src="' . $team_one_logo . '"></div>');
+$page->addStringBodyElement('<div class="col-sm-6"><div class="jumbotron"><div class="row">');
+$page->addStringBodyElement('<div class="col-sm-2"></div>');
+$page->addStringBodyElement('<div class="col-sm-2">' . $match['team_one_ft'] . '</div>');
+$page->addStringBodyElement('<div class="col-sm-4"></div>');
+$page->addStringBodyElement('<div class="col-sm-2">' . $match['team_two_ft'] . '</div>');
+$page->addStringBodyElement('<div class="col-sm-2"></div></div>');
+$page->addStringBodyElement('<div class="col-sm-3"><img class="center-block" src="' . $team_two_logo . '"></div>');
+$page->addStringBodyElement('</div>');
+
+$page->display();
