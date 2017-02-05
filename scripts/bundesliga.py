9f9865e295a2a6eb15028066fc11f165927362e3
Hermann Krumrey <hermann@krumreyh.com>
2017-02-05 13:55:10 +0000

    Fix Shortname of teams

--- scripts/bundesliga.py
+++ scripts/bundesliga.py
@@ -64,6 +64,8 @@ def update_db_teams(data, db):
 
         if not team["TeamId"] in current_data:
 
+            shortname = team["ShortName"] if team["ShortName"] != "" else team["TeamName"]
+
             stmt = db.cursor()
             stmt.execute("INSERT INTO teams (id, name, shortname, icon)"\
                          "VALUES (%s, %s, %s, %s);",
