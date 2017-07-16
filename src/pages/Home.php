4c27921681931937697efbc855a61661e86653af
Hermann Krumrey <hermann@krumreyh.com>
2017-07-17 00:19:25 +0200

    Add missing license headers

--- src/pages/Home.php
+++ src/pages/Home.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ * Copyright Hermann Krumrey <hermann@krumreyh.com> 2017
+ *
+ * This file is part of bundesliga_tippspiel.
+ *
+ * bundesliga_tippspiel is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * bundesliga_tippspiel is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with bundesliga_tippspiel. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 namespace bundesliga_tippspiel;
 use chameleon\HtmlTemplate;
