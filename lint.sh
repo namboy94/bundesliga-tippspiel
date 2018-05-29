09d817129522062c35fd99d7336dc5f319ad27e2
Hermann Krumrey <hermann@krumreyh.com>
2018-05-29 11:08:57 +0200

    Start integrating ci-scripts

--- lint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ -z "$SHOW_LINT_RESULTS" ]; then
-    php vendor/phpcheckstyle/phpcheckstyle/run.php --src src --src test --config checkstyle.xml
-else
-    php vendor/phpcheckstyle/phpcheckstyle/run.php --src src --src test --config checkstyle.xml || firefox style-report/index.html
-fi
