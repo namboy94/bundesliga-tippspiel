24c06bec90ccbec4c580f98592d6cc22a109d8bd
Hermann Krumrey <hermann@krumreyh.com>
2018-08-20 09:27:39 +0200

    Adjust SQL schema for mysql

--- bundesliga_tippspiel/models/match_data/Team.py
+++ bundesliga_tippspiel/models/match_data/Team.py
@@ -45,7 +45,7 @@ class Team(db.Model):
     Example: FC Bayern München
     """
 
-    short_name = db.Column(db.String(255), nullable=False, unique=True)
+    short_name = db.Column(db.String(16), nullable=False, unique=True)
     """
     The shortened version of the team's name. Has to be unique.
     Example: Bayern
@@ -57,12 +57,12 @@ class Team(db.Model):
     Example: FCB
     """
 
-    icon_svg = db.Column(db.String(255), nullable=False, unique=True)
+    icon_svg = db.Column(db.String(255), nullable=False)
     """
     The URL of an image file representing the team's logo in SVG format
     """
 
-    icon_png = db.Column(db.String(255), nullable=False, unique=True)
+    icon_png = db.Column(db.String(255), nullable=False)
     """
     The URL of an image file representing the team's logo in PNG format
     """
