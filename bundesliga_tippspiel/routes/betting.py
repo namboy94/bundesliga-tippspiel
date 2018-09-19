0762bbd1ad472a1ce186ca59130330fe7da0f36e
Hermann Krumrey <hermann@krumreyh.com>
2018-09-19 17:24:25 +0200

    Fix matchday resolution

--- bundesliga_tippspiel/routes/betting.py
+++ bundesliga_tippspiel/routes/betting.py
@@ -51,6 +51,8 @@ def bets(matchday: Optional[int] = None):
             matchday=matchday
         ).execute()["matches"]
 
+        print(matchday_matches)
+
         betmap = {}
         for _match in matchday_matches:
             betmap[_match.id] = None
