d1fea36ce2a7070845ff5512399df22cd1876a1e
Hermann Krumrey <hermann@krumreyh.com>
2018-09-19 17:49:54 +0200

    Fix test for charts once more

--- bundesliga_tippspiel/test/utils/TestChartData.py
+++ bundesliga_tippspiel/test/utils/TestChartData.py
@@ -42,12 +42,14 @@ class TestChartData(_TestFramework):
         match_one = Match(
             home_team=team_one, away_team=team_two,
             matchday=1, kickoff="2019-01-01:01:02:03",
-            started=True, finished=True
+            started=True, finished=True,
+            home_current_score=1, away_current_score=2
         )
         match_two = Match(
             home_team=team_one, away_team=team_two,
             matchday=2, kickoff="2019-01-01:01:02:03",
-            started=False, finished=False
+            started=False, finished=False,
+            home_current_score=0, away_current_score=0
         )
         self.db.session.add(match_one)
         self.db.session.add(match_two)
